36825caee3aba4d3451836c840731576a74ad381
Michal Dengusiak <tassimulation@gmail.com>
2025-06-11 12:10:28 +0200

    Update SaturationVapourPressure.cs

--- SAM_Mollier/Tests/SaturationVapourPressure.cs
+++ SAM_Mollier/Tests/SaturationVapourPressure.cs
@@ -10,13 +10,13 @@
             // Calculate the saturation vapour pressure using both methods
             double calculatedSaturationVapourPressure = Core.Mollier.Query.SaturationVapourPressure(temperature);
             double calculatedSaturationVapourPressure_IAPWS = Core.Mollier.Query.SaturationVapourPressure_IAPWS(temperature);
-            Validation.Modify.Report(calculatedSaturationVapourPressure, calculatedSaturationVapourPressure_IAPWS,0.01);
+            Validation.Modify.Report(calculatedSaturationVapourPressure, calculatedSaturationVapourPressure_IAPWS);
         }
 
         public static IEnumerable<object[]> GetTemperatureData()
         {
             // Generate test data for temperatures from -20 to 99
-            for (int i = -50; i < 200; i++)
+            for (int i = -50; i < 100; i++)
             {
                 yield return new object[] { i };
             }
