0413df7d34ad0dd09e8234035b832e9d8be996a1
Michal Dengusiak <tassimulation@gmail.com>
2025-06-11 11:31:36 +0200

    Create SaturationVapourPressure.cs

---
+++ SAM_Mollier/Tests/SaturationVapourPressure.cs
@@ -0,0 +1,25 @@
+﻿namespace SAM.Mollier
+{
+    /// Validates the calculated SaturationVapourPressure between Glueck and IAPWS.
+    public sealed partial class Tests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTemperatureData), DynamicDataSourceType.Method)]
+        public void SaturationVapourPressure(int temperature)
+        {
+            // Calculate the saturation vapour pressure using both methods
+            double calculatedSaturationVapourPressure = Core.Mollier.Query.SaturationVapourPressure(temperature);
+            double calculatedSaturationVapourPressure_IAPWS = Core.Mollier.Query.SaturationVapourPressure_IAPWS(temperature);
+            Validation.Modify.Report(calculatedSaturationVapourPressure, calculatedSaturationVapourPressure_IAPWS,0.01);
+        }
+
+        public static IEnumerable<object[]> GetTemperatureData()
+        {
+            // Generate test data for temperatures from -20 to 99
+            for (int i = -50; i < 200; i++)
+            {
+                yield return new object[] { i };
+            }
+        }
+    }
+}
